ba8fc162f430e92766e45eb01287364c6a5c043d
HyuEstafet <hyulya.gyuler@estafet.com>
2022-05-18 14:59:41 +0300

    commiting changes in the pom and updates on the JUnit task

--- src/test/java/CalculatorTesting.java
+++ src/test/java/CalculatorTesting.java
@@ -1,26 +1,38 @@
-import org.junit.Test;
 import org.junit.jupiter.api.*;
 
 public class CalculatorTesting {
 
     private CalculatorClass calculator;
 
+    @BeforeEach
+    public void setCalculator() {
+        calculator = new CalculatorClass();
+    }
+
     @Test
     public void addCorrectPass () {
-        CalculatorClass calculator = new CalculatorClass();
-        Assertions.assertEquals(8, calculator.add(3,5));
+        Assertions.assertEquals(8, calculator.add(3,5)); // this will pass
     }
 
     @Test
     public void addWrongPass () {
-        CalculatorClass calculator = new CalculatorClass();
-        Assertions.assertNotEquals(5.2, calculator.add(1.2,3.0));
+        Assertions.assertNotEquals(5.2, calculator.add(1.2,3.0)); // this will pass
     }
 
+    @Disabled ("Testing the ignore functionality in JUnit 5")
     @Test
     public void addWrongFail () {
-        CalculatorClass calculator = new CalculatorClass();
-        Assertions.assertEquals(5.2, calculator.add(1.2,3.0));
+        Assertions.assertEquals(5.2, calculator.add(1.2,3.0)); // this will fail
+    }
+
+    @AfterEach // to run after each test
+    public void doneTests () {
+        System.out.println("The test is done");
+    }
+
+    @AfterAll // to run once after all tests are done
+    static  void doneAll () {
+        System.out.println("Finished");
     }
 
 }
